ce8cf31967f9b732ba97a2f1b877208db0caa932
DanceMore <dancemore@protonmail.com>
2023-08-27 21:41:29 -0700

    document PlaylistTags object

--- lib/jukectl/playlist_tags.rb
+++ lib/jukectl/playlist_tags.rb
@@ -1,3 +1,12 @@
+# simple Structure to hold the internal state of "which Tags does the jukebox select from"
+# it is based on Set Maths and/or Boolean Logic. fields are Arrays or comma-strings in JSON
+# so you can have multiple tags active or excluded at once.
+#
+# `any`: any Song with any of these Tags can be played
+# `not`: Set() logic exclude Songs with any of these tags from being played
+#
+# default sets to {any: jukebox, not: explicit}, which I consider a sane default for radio-heads :)
+
 class PlaylistTags
   def initialize
     @tags = {}
